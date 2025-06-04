88949c9816c82cdd53d683715bbf825444a4a0e0
Rohan Raj <rjha09307@gmail.com>
2025-06-04 17:58:12 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: ['react-toastify'], // Add 'react-toastify' as an external
+    },
+  },
+});
 
 // https://vite.dev/config/
 export default defineConfig({
